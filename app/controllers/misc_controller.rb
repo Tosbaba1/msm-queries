fdcae7a051b13e383a2e76dda8833b25129935de
Tosbaba1 <tosan.agbeyegbe@gmail.com>
2025-04-29 20:27:51 +0000

    Created Actors, Movies, and Directors pages

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,4 +2,37 @@ class MiscController < ApplicationController
   def homepage
     render({ :template => "misc_templates/home"})
   end
+
+  def movies
+    @list_of_movies = Movie.all
+    render({ :template => "misc_templates/movie"})
+  end
+
+  def directors
+    @list_of_directors = Director.all
+    render({ :template => "misc_templates/director"})
+  end
+
+  def show
+    @director = Director.find(params[:the_id])
+    render ({ template: "misc_templates/show"})
+  end
+
+  def show_movies
+    @movie = Movie.find(params[:the_id])
+    render ({ template: "misc_templates/show_movies"})
+  end
+
+  def youngest
+    render({ :template => "misc_templates/young"})
+  end
+
+  def eldest
+    render({ :template => "misc_templates/old"})
+  end
+
+  def actors
+    @list_of_actors = Actor.all
+    render({ :template => "misc_templates/actor"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+
+  get("/movies", { :controller => "misc", :action => "movies" })
+
+  get("/directors", { :controller => "misc", :action => "directors" })
+
+  get("/directors/youngest", { :controller => "misc", :action => "youngest" })
+
+  get("/directors/eldest", { :controller => "misc", :action => "eldest" })
+
+  get("/actors", { :controller => "misc", :action => "actors" })
+
+  get("/directors/:the_id", { :controller => "misc", :action => "show"})
+
+  get("/movies/:the_id", { :controller => "misc", :action => "show_movies"})
+
+  get("/actors/:the_id", { :controller => "misc", :action => "show_actors"})
 end
